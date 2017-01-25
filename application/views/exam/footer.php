b27c244c0c57f3ef634753e29e831c70c4ccd422
Xqf5683 <734086455@qq.com>
2017-01-25 21:55:47 +0800

    huan le

--- application/views/exam/footer.php
+++ application/views/exam/footer.php
@@ -1,6 +1,12 @@
 
-	<script src="<?php echo base_url(); ?>js/jquery.js"></script>
-	<script src="<?php echo base_url(); ?>js/index.js"></script>
+	<script src="<?php echo base_url(); ?>js/notie.js"></script>
+	<script>
+		function score() {
+			notie.confirm('你的此次考试成绩为：<strong>100分！</strong>', 'Yes', 'Cancel', function() {
+				notie.alert(1, 'Good choice!', 2);
+			});
+		}
+	</script>
 
 	<script src="<?php echo base_url(); ?>js/jquery-1.8.3.min.js"></script>
 	<script src="<?php echo base_url(); ?>js/answer.js"></script>
