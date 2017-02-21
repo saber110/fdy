c5fd76e5e4c0a013fc7b87427e0708592282e766
saber110 <huhaobin110@gmail.com>
2017-02-21 12:52:36 +0800

    update somethings

--- application/models/Exam_model.php
+++ application/models/Exam_model.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once APPPATH."../debug/chromephp/ChromePhp.php";
+require_once FCPATH."debug/chromephp/ChromePhp.php";
 
 /**
  * 抽题及校对
